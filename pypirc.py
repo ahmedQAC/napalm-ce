0d1c613ac92529ebf5ef4f2192b398f45686ccb1
ahmedqac <ahmed1645@hotmail.com>
2020-02-21 10:21:45 +0000

    added pypirc file

---
+++ pypirc.py
@@ -0,0 +1,5 @@
+[distutils]
+Index-servers =
+  NetworkSystemsDO
+[NetworkSystemsDO]
+Repository = https://pkgs.dev.azure.com/NetworkSystems/974dda80-6cd6-48c7-906c-f704204f7334/_packaging/NetworkSystemsDO/pypi/upload
